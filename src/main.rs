afdadb656b44429445d8bed34b01f9af81bbd6a1
Iku Iwasa <iku.iwasa@gmail.com>
2021-08-21 23:02:31 +0900

    Implement "init" sub command for bash only

--- src/main.rs
+++ src/main.rs
@@ -14,8 +14,9 @@ USAGE:
     {} <SUBCOMMAND>
 
 SUBCOMMANDS:
-    record <COMMAND_LINE>
     history
+    init bash
+    record <COMMAND_LINE>
     stats
 ",
         env!("CARGO_BIN_NAME"),
@@ -25,26 +26,38 @@ SUBCOMMANDS:
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    rireq()
+}
+
+fn rireq() -> Result<(), Box<dyn std::error::Error>> {
     let mut args = env::args().skip(1);
     if let Some(ref subcmd) = args.next() {
         if subcmd == "history" {
             let db = Db::open()?;
             db.history()?;
+            return Ok(());
+        } else if subcmd == "init" {
+            if let Some(shell) = args.next() {
+                if shell == "bash" {
+                    println!("{}", include_str!("../script/init.bash"));
+                    return Ok(());
+                } else {
+                    eprintln!("Unknown shell: {} (only \"bash\" supported)", shell);
+                    return Ok(());
+                }
+            }
         } else if subcmd == "record" {
             if let Some(cmdline) = args.next() {
                 let db = Db::open()?;
                 db.record(CmdRecord::new(cmdline))?;
             }
+            return Ok(());
         } else if subcmd == "stats" {
             let db = Db::open()?;
             db.stats()?;
-        } else {
-            usage();
-            exit(1);
+            return Ok(())
         }
-    } else {
-        usage();
-        exit(1);
     }
-    Ok(())
+    usage();
+    exit(1);
 }
